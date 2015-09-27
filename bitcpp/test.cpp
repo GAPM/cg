f92da79916633ff01b66991dc1afa8136d17f38a
Simon Oroño <sron611@gmail.com>
2015-09-27 09:32:46 -0430

    Removed bitmatrix class

--- bitcpp/test.cpp
+++ bitcpp/test.cpp
@@ -4,44 +4,24 @@
 #include "bitmatrix.h"
 
 #define MAXARRAY 100000000
-#define MAXMATRIX 10000
 
 int main() {
     std::unique_ptr<bitarray> ba (new bitarray(MAXARRAY));
-    std::unique_ptr<bitmatrix> bm (new bitmatrix(MAXMATRIX, MAXMATRIX));
 
     int i;
-    int j;
+    std::size_t size = ba->size();
 
-    for (i = 0; i < MAXARRAY; ++i) {
+    for (i = 0; i < size; ++i) {
         if (i % 2 == 0) {
             ba->set(i, true);
         }
     }
 
-    for (i = 0; i < MAXARRAY; ++i) {
+    for (i = 0; i < size; ++i) {
         if (i % 2 == 0) {
             assert(ba->get(i));
         } else {
             assert(!ba->get(i));
-        }
-    }
-
-    for (i = 0; i < MAXMATRIX; ++i) {
-        for (j = 0; j < MAXMATRIX; ++j) {
-            if (i == j) {
-                bm->set(i, j, true);
-            }
-        }
-    }
-
-    for (i = 0; i < MAXMATRIX; ++i) {
-        for (j = 0; j < MAXMATRIX; ++j) {
-            if (i == j) {
-                assert(bm->get(i, j));
-            } else {
-                assert(!bm->get(i, j));
-            }
         }
     }
 }
