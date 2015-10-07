54ef182292643b3024f52a50e7501e8192d2d991
Simon Oroño <sron611@gmail.com>
2015-10-06 20:57:13 -0430

    Better format

--- bitcpp/bitarray.cpp
+++ bitcpp/bitarray.cpp
@@ -4,25 +4,23 @@
 #include <cmath>
 
 bitarray::bitarray(std::size_t size) {
-    std::size_t bytes = ceil(size / CHAR_BIT);
-    this->_size = size;
-    this->array = std::unique_ptr<byte_t[]>(new byte_t[bytes]());
+  std::size_t bytes = ceil(size / CHAR_BIT);
+  this->_size = size;
+  this->array = std::unique_ptr<byte_t[]>(new byte_t[bytes]());
 }
 
 bitarray::~bitarray() {}
 
 bool bitarray::get(std::size_t i) {
-    return this->array[i / CHAR_BIT] & (1 << (i % CHAR_BIT));
+  return this->array[i / CHAR_BIT] & (1 << (i % CHAR_BIT));
 }
 
 void bitarray::set(std::size_t i, bool v) {
-    if (v) {
-        this->array[i / CHAR_BIT] |= (1 << (i % CHAR_BIT));
-    } else {
-        this->array[i / CHAR_BIT] &= ~(1 << (i % CHAR_BIT));
-    }
+  if (v) {
+    this->array[i / CHAR_BIT] |= (1 << (i % CHAR_BIT));
+  } else {
+    this->array[i / CHAR_BIT] &= ~(1 << (i % CHAR_BIT));
+  }
 }
 
-std::size_t bitarray::size() {
-    return this->_size;
-}
+std::size_t bitarray::size() { return this->_size; }
--- bitcpp/test.cpp
+++ bitcpp/test.cpp
@@ -6,22 +6,22 @@
 #define MAXARRAY 100000000
 
 int main() {
-    std::unique_ptr<bitarray> ba (new bitarray(MAXARRAY));
+  std::unique_ptr<bitarray> ba(new bitarray(MAXARRAY));
 
-    int i;
-    std::size_t size = ba->size();
+  int i;
+  std::size_t size = ba->size();
 
-    for (i = 0; i < size; ++i) {
-        if (i % 2 == 0) {
-            ba->set(i, true);
-        }
+  for (i = 0; i < size; ++i) {
+    if (i % 2 == 0) {
+      ba->set(i, true);
     }
+  }
 
-    for (i = 0; i < size; ++i) {
-        if (i % 2 == 0) {
-            assert(ba->get(i));
-        } else {
-            assert(!ba->get(i));
-        }
+  for (i = 0; i < size; ++i) {
+    if (i % 2 == 0) {
+      assert(ba->get(i));
+    } else {
+      assert(!ba->get(i));
     }
+  }
 }
