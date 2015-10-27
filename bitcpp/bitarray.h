29efb894efda200e7687d72580c8827ec5254eba
Simon Oroño <sron611@gmail.com>
2015-10-26 21:10:40 -0430

    Minor fixes:
    -Better and unified formatting for C/C++ files
    -Added Java and Maven garbage and build to .gitignore

--- bitcpp/bitarray.h
+++ bitcpp/bitarray.h
@@ -7,17 +7,17 @@ typedef unsigned char byte_t;
 
 class bitarray {
 public:
-  bitarray(std::size_t);
-  ~bitarray();
+    bitarray(std::size_t);
+    ~bitarray();
 
-  bool get(std::size_t);
-  void set(std::size_t, bool);
+    bool get(std::size_t);
+    void set(std::size_t, bool);
 
-  std::size_t size();
+    std::size_t size();
 
 private:
-  std::size_t _size;
-  std::unique_ptr<byte_t[]> array;
+    std::size_t _size;
+    std::unique_ptr<byte_t[]> array;
 };
 
 #endif
--- compare/main.cpp
+++ compare/main.cpp
@@ -12,73 +12,73 @@
 using namespace std;
 
 void measure(void (*f)(), string name) {
-  auto start = chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
 
-  f();
+    f();
 
-  auto end = chrono::high_resolution_clock::now();
-  auto dur = chrono::duration_cast<chrono::milliseconds>(end - start);
-  cout << name << ": " << dur.count() << " ms" << endl;
+    auto end = chrono::high_resolution_clock::now();
+    auto dur = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << name << ": " << dur.count() << " ms" << endl;
 }
 
 inline void testBitArray() {
-  bitarray ba(MAX);
-  size_t i;
-  size_t size = ba.size();
+    bitarray ba(MAX);
+    size_t i;
+    size_t size = ba.size();
 
-  for (i = 0; i < size; ++i) {
-    if (i % 2 == 0) {
-      ba.set(i, true);
+    for (i = 0; i < size; ++i) {
+        if (i % 2 == 0) {
+            ba.set(i, true);
+        }
     }
-  }
 
-  for (i = 0; i < size; ++i) {
-    if (i % 2 == 0) {
-      assert(ba.get(i));
-    } else {
-      assert(!ba.get(i));
+    for (i = 0; i < size; ++i) {
+        if (i % 2 == 0) {
+            assert(ba.get(i));
+        } else {
+            assert(!ba.get(i));
+        }
     }
-  }
 }
 
 inline void testRawArray() {
-  unique_ptr<bool[]> ba(new bool[MAX]());
+    unique_ptr<bool[]> ba(new bool[MAX]());
 
-  for (size_t i = 0; i < MAX; ++i) {
-    if (i % 2 == 0) {
-      ba[i] = true;
+    for (size_t i = 0; i < MAX; ++i) {
+        if (i % 2 == 0) {
+            ba[i] = true;
+        }
     }
-  }
 
-  for (size_t i = 0; i < MAX; ++i) {
-    if (i % 2 == 0) {
-      assert(ba[i]);
-    } else {
-      assert(!ba[i]);
+    for (size_t i = 0; i < MAX; ++i) {
+        if (i % 2 == 0) {
+            assert(ba[i]);
+        } else {
+            assert(!ba[i]);
+        }
     }
-  }
 }
 
 inline void testStdVector() {
-  vector<bool> ba(MAX);
+    vector<bool> ba(MAX);
 
-  for (size_t i = 0; i < ba.size(); ++i) {
-    if (i % 2 == 0) {
-      ba[i] = true;
+    for (size_t i = 0; i < ba.size(); ++i) {
+        if (i % 2 == 0) {
+            ba[i] = true;
+        }
     }
-  }
 
-  for (size_t i = 0; i < MAX; ++i) {
-    if (i % 2 == 0) {
-      assert(ba[i]);
-    } else {
-      assert(!ba[i]);
+    for (size_t i = 0; i < MAX; ++i) {
+        if (i % 2 == 0) {
+            assert(ba[i]);
+        } else {
+            assert(!ba[i]);
+        }
     }
-  }
 }
 
 int main(void) {
-  measure(testBitArray, "bitarray");
-  measure(testRawArray, "raw array");
-  measure(testStdVector, "std vector");
+    measure(testBitArray, "bitarray");
+    measure(testRawArray, "raw array");
+    measure(testStdVector, "std vector");
 }
