344545b863f042a5a83e0a8b30b6b333010e6628
Simón Oroño <sron611@gmail.com>
2016-05-12 22:38:01 -0400

    Default value generation for variables

---
+++ cgc/src/main/kotlin/sron/cg/compiler/phase/generation/helper/Var.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 Simón Oroño & La Universidad del Zulia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sron.cg.compiler.phase.generation.helper
+
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes.FCONST_0
+import org.objectweb.asm.Opcodes.ICONST_0
+import sron.cg.type.Type
+
+fun generateDefault(mv: MethodVisitor, type: Type) {
+    when (type) {
+        Type.int -> mv.visitInsn(ICONST_0)
+        Type.float -> mv.visitInsn(FCONST_0)
+        Type.bool -> mv.visitInsn(ICONST_0)
+        Type.string -> mv.visitLdcInsn("")
+        Type.graph -> {
+
+        }
+        Type.digraph -> {
+
+        }
+        else -> {
+        }
+    }
+}
