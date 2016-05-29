df3a49d5a4cf28d4b1e571f98576fc594f12f830
Simón Oroño <sron611@gmail.com>
2016-05-28 21:15:46 -0400

    Fixed wrong output in DiGraph.toString

--- cgrt/src/main/java/sron/cg/lang/DiGraph.java
+++ cgrt/src/main/java/sron/cg/lang/DiGraph.java
@@ -88,8 +88,11 @@ public class DiGraph implements IGraph {
 
     @Override
     public String toString() {
-        String res = "{";
         List<String> edges = new ArrayList<>();
+
+        String res = "digraph (";
+        res += Integer.toString(size);
+        res += ") {";
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
